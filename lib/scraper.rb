c82bdf14c8e61019a0667029d10bd93beafc7676
Anthony <anthonylnguyen97@gmail.com>
2018-02-26 19:17:47 -0800

    scraper commit

---
+++ lib/scraper.rb
@@ -0,0 +1,44 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'openssl'
+
+class Scraper
+	attr_accessor :parse_page
+
+	def initialize
+		doc = HTTParty.get("http://www.cpp.edu/events/")
+		#doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
+		@parse_page ||= Nokogiri::HTML(doc)
+	end
+
+	def get_cal
+		cals = item_container.css(".padding").css("h3").children.map { |cal| cal.text }.compact
+	end
+
+	def get_date
+		dates = item_container.css(".padding").css("h4").children.map { |cal| cal.text }.compact
+	end
+
+	def get_info
+		infos = item_container.css(".padding").css("p").children.map { |cal| cal.text }.compact
+	end
+
+
+	def item_container
+		parse_page.css(".event-intro")
+	end
+
+	scraper = Scraper.new
+	cals = scraper.get_cal
+	dates = scraper.get_date
+	infos = scraper.get_info
+
+
+	(0...cals.size).each do |index|
+		puts "===== Event: #{index+1} ====="
+		puts "Event: #{cals[index]}"
+		puts "Date: #{dates[index]}"
+		puts "Info: #{infos[index]}"
+	end
+
+end
