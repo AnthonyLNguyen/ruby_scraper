f588faca75482d8bea453e95b0aec03c71706c10
Anthony Nguyen <anthonylnguyen97@gmail.com>
2018-03-01 14:40:34 -0800

    interactive

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,42 @@
+#!/bin/ruby
+
 require 'httparty'
 require 'nokogiri'
 require 'openssl'
+
+class Page
+    def initialize(url)
+        @url = url
+    end
+end
+
+class Container
+    attr_accessor :parser
+
+    def initialize(outer)
+		page = HTTParty.get("http://www.cpp.edu/events/")
+		@parser ||= Nokogiri::HTML(page)
+        @outer = parser.css(outer)
+        @inners = []
+    end
+    def add_inner(inner)
+        @inners << inner
+    end
+    def get_next(inner)
+        @outer.css(inner)
+    end
+    def get_element(index)
+        parse_tags(ARGV[index])
+    end
+    def parse_tags(tag)
+        result = @outer
+        arr = tag.split(" ")
+        arr.each do|a|
+            result = result.css(a)
+        end
+        return result
+    end
+end
 
 class Scraper
 	# shortcut for writing
@@ -13,44 +49,30 @@ class Scraper
 	#  	@parser
 	# end
 	#
-	attr_accessor :parser
-
-	def initialize
+    attr_accessor :parser
+    def initialize
 		page = HTTParty.get("http://www.cpp.edu/events/")
-		# use @ for instance variable which is avaible to all methods in the class
+        # use @ for instance variable which is avaible to all methods in the class
 		# a ||= b is equivalent to a || a = b
 		@parser ||= Nokogiri::HTML(page)
 	end
 
 	#Define container to search through
-	def container
-		parser.css(".event-intro")
-	end
-
-	def get_events
-		# compact removes all instances of nil in the array
-		events = container.css(".padding").css("h3").children.map { |event| event.text }.compact
-	end
-
-	def get_date
-		dates = container.css(".padding").css("h4").children.map { |date| date.text }.compact
-	end
-
-	def get_info
-		infos = container.css(".padding").css("p").children.map { |info| info.text }.compact
-	end
-
-	scraper = Scraper.new
-	events = scraper.get_events
-	dates = scraper.get_date
-	infos = scraper.get_info
+    def container
+        outer = Container.new(".event-intro")
+        elements = outer.get_element(0).children.map { |elements| elements.text }.compact
+        (1...ARGV.size).each do |index|
+            elements = outer.get_element(index).children.map { |elements| elements+"\n"+elements.text }.compact
+        end
+        return elements
+    end
+    sc = Scraper.new
+    elements = sc.container
 
 	# loop from 0 to events size
-	(0...events.size).each do |index|
-		puts "\n===== Event: #{index+1} ====="
-		puts "Event: #{events[index]}"
-		puts "Date: #{dates[index]}"
-		puts "Info: #{infos[index]}"
+	(0...elements.size).each do |index|
+		puts "\n===== Element: #{index+1} ====="
+		puts "#{elements[index]}"
 	end
-
 end
+
